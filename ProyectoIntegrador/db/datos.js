063d430966330a3a95ddf7c822dcfb416469111b
Nico <nicolasrusso004@gmail.com>
2024-04-08 11:58:39 -0300

    Mantener el header y footer comunes en todas las páginas del sitio.

---
+++ ProyectoIntegrador/db/datos.js
@@ -0,0 +1,251 @@
+let datosApp = {
+    usuario: {
+      email: "juanperez@mail.com",
+      usuario: "Juan Pérez",
+      contraseña: "12345678",
+      fechaNacimiento: "1990-01-01",
+      nroDocumento: 12345678,
+      fotoPerfil: "/imagenes/perfiles/usuario123.jpg",
+    },
+
+    productos: [
+        {
+            imagen: "/imagenes/products/default-image.png",
+            nombre: "Producto 1",
+            descripcion: "Descripción del producto 1.",
+            comentarios: [
+              {
+                nombreUsuario: "Usuario Ejemplo",
+                texto: "¡Me encanta este producto!",
+                imagenPerfil: "/imagenes/perfiles/ejemploPerfil1.jpg"
+              },
+              {
+                nombreUsuario: "Usuario Ejemplo",
+                texto: "¡Me fascina este producto!",
+                imagenPerfil: "/imagenes/perfiles/ejemploPerfil1.jpg"
+              },
+
+              {
+                nombreUsuario: "Usuario Ejemplo",
+                texto: "¡Me divierte este producto!",
+                imagenPerfil: "/imagenes/perfiles/ejemploPerfil1.jpg"
+              }
+            ]},
+
+
+      {
+        imagen: "/imagenes/productos/prod2.jpg",
+        nombre: "Producto 2",
+        descripcion: "Descripción del producto 2.",
+        comentarios: [
+            {
+              nombreUsuario: "Usuario Ejemplo",
+              texto: "¡Me encanta este producto!",
+              imagenPerfil: "/imagenes/perfiles/ejemploPerfil1.jpg"
+            },
+            {
+              nombreUsuario: "Usuario Ejemplo",
+              texto: "¡Me fascina este producto!",
+              imagenPerfil: "/imagenes/perfiles/ejemploPerfil1.jpg"
+            },
+
+            {
+              nombreUsuario: "Usuario Ejemplo",
+              texto: "¡Me divierte este producto!",
+              imagenPerfil: "/imagenes/perfiles/ejemploPerfil1.jpg"
+            }
+          ]
+      },
+
+      {
+        imagen: "/imagenes/productos/prod3.jpg",
+        nombre: "Producto 3",
+        descripcion: "Descripción del producto 3.",
+        comentarios: [
+            {
+              nombreUsuario: "Usuario Ejemplo",
+              texto: "¡Me encanta este producto!",
+              imagenPerfil: "/imagenes/perfiles/ejemploPerfil1.jpg"
+            },
+            {
+              nombreUsuario: "Usuario Ejemplo",
+              texto: "¡Me fascina este producto!",
+              imagenPerfil: "/imagenes/perfiles/ejemploPerfil1.jpg"
+            },
+
+            {
+              nombreUsuario: "Usuario Ejemplo",
+              texto: "¡Me divierte este producto!",
+              imagenPerfil: "/imagenes/perfiles/ejemploPerfil1.jpg"
+            }
+          ]
+      },
+      {
+        imagen: "/imagenes/productos/prod4.jpg",
+        nombre: "Producto 4",
+        descripcion: "Descripción del producto 4.",
+        comentarios: [
+            {
+              nombreUsuario: "Usuario Ejemplo",
+              texto: "¡Me encanta este producto!",
+              imagenPerfil: "/imagenes/perfiles/ejemploPerfil1.jpg"
+            },
+            {
+              nombreUsuario: "Usuario Ejemplo",
+              texto: "¡Me fascina este producto!",
+              imagenPerfil: "/imagenes/perfiles/ejemploPerfil1.jpg"
+            },
+
+            {
+              nombreUsuario: "Usuario Ejemplo",
+              texto: "¡Me divierte este producto!",
+              imagenPerfil: "/imagenes/perfiles/ejemploPerfil1.jpg"
+            }
+          ]
+      },
+      {
+        imagen: "/imagenes/productos/prod5.jpg",
+        nombre: "Producto 5",
+        descripcion: "Descripción del producto 5.",
+        comentarios: [
+            {
+              nombreUsuario: "Usuario Ejemplo",
+              texto: "¡Me encanta este producto!",
+              imagenPerfil: "/imagenes/perfiles/ejemploPerfil1.jpg"
+            },
+            {
+              nombreUsuario: "Usuario Ejemplo",
+              texto: "¡Me fascina este producto!",
+              imagenPerfil: "/imagenes/perfiles/ejemploPerfil1.jpg"
+            },
+
+            {
+              nombreUsuario: "Usuario Ejemplo",
+              texto: "¡Me divierte este producto!",
+              imagenPerfil: "/imagenes/perfiles/ejemploPerfil1.jpg"
+            }
+          ]
+      },
+      {
+        imagen: "/imagenes/productos/prod6.jpg",
+        nombre: "Producto 6",
+        descripcion: "Descripción del producto 6.",
+        comentarios: [
+            {
+              nombreUsuario: "Usuario Ejemplo",
+              texto: "¡Me encanta este producto!",
+              imagenPerfil: "/imagenes/perfiles/ejemploPerfil1.jpg"
+            },
+            {
+              nombreUsuario: "Usuario Ejemplo",
+              texto: "¡Me fascina este producto!",
+              imagenPerfil: "/imagenes/perfiles/ejemploPerfil1.jpg"
+            },
+
+            {
+              nombreUsuario: "Usuario Ejemplo",
+              texto: "¡Me divierte este producto!",
+              imagenPerfil: "/imagenes/perfiles/ejemploPerfil1.jpg"
+            }
+          ]
+      },
+      {
+        imagen: "/imagenes/productos/prod7.jpg",
+        nombre: "Producto 7",
+        descripcion: "Descripción del producto 7.",
+        comentarios: [
+            {
+              nombreUsuario: "Usuario Ejemplo",
+              texto: "¡Me encanta este producto!",
+              imagenPerfil: "/imagenes/perfiles/ejemploPerfil1.jpg"
+            },
+            {
+              nombreUsuario: "Usuario Ejemplo",
+              texto: "¡Me fascina este producto!",
+              imagenPerfil: "/imagenes/perfiles/ejemploPerfil1.jpg"
+            },
+
+            {
+              nombreUsuario: "Usuario Ejemplo",
+              texto: "¡Me divierte este producto!",
+              imagenPerfil: "/imagenes/perfiles/ejemploPerfil1.jpg"
+            }
+          ]
+      },
+      {
+        imagen: "/imagenes/productos/prod8.jpg",
+        nombre: "Producto 8",
+        descripcion: "Descripción del producto 8.",
+        comentarios: [
+            {
+              nombreUsuario: "Usuario Ejemplo",
+              texto: "¡Me encanta este producto!",
+              imagenPerfil: "/imagenes/perfiles/ejemploPerfil1.jpg"
+            },
+            {
+              nombreUsuario: "Usuario Ejemplo",
+              texto: "¡Me fascina este producto!",
+              imagenPerfil: "/imagenes/perfiles/ejemploPerfil1.jpg"
+            },
+
+            {
+              nombreUsuario: "Usuario Ejemplo",
+              texto: "¡Me divierte este producto!",
+              imagenPerfil: "/imagenes/perfiles/ejemploPerfil1.jpg"
+            }
+          ]
+      },
+      {
+        imagen: "/imagenes/productos/9.jpg",
+        nombre: "Producto 9",
+        descripcion: "Descripción del producto 9.",
+        comentarios: [
+            {
+              nombreUsuario: "Usuario Ejemplo",
+              texto: "¡Me encanta este producto!",
+              imagenPerfil: "/imagenes/perfiles/ejemploPerfil1.jpg"
+            },
+            {
+              nombreUsuario: "Usuario Ejemplo",
+              texto: "¡Me fascina este producto!",
+              imagenPerfil: "/imagenes/perfiles/ejemploPerfil1.jpg"
+            },
+
+            {
+              nombreUsuario: "Usuario Ejemplo",
+              texto: "¡Me divierte este producto!",
+              imagenPerfil: "/imagenes/perfiles/ejemploPerfil1.jpg"
+            }
+          ]
+      },
+      {
+        imagen: "/imagenes/productos/prod10.jpg",
+        nombre: "Producto 10",
+        descripcion: "Descripción del producto 10.",
+        comentarios: [
+            {
+              nombreUsuario: "Usuario Ejemplo",
+              texto: "¡Me encanta este producto!",
+              imagenPerfil: "/imagenes/perfiles/ejemploPerfil1.jpg"
+            },
+            {
+              nombreUsuario: "Usuario Ejemplo",
+              texto: "¡Me fascina este producto!",
+              imagenPerfil: "/imagenes/perfiles/ejemploPerfil1.jpg"
+            },
+
+            {
+              nombreUsuario: "Usuario Ejemplo",
+              texto: "¡Me divierte este producto!",
+              imagenPerfil: "/imagenes/perfiles/ejemploPerfil1.jpg"
+            }
+          ]
+      },
+    
+
+
+    ],
+
+
+  };
+  
