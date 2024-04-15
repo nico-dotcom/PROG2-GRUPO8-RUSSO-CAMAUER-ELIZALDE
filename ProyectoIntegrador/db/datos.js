fcceded496d8391c7a9bc97df7008661ddac6981
bernicamauer <bcamauer@udesa.edu.ar>
2024-04-15 10:01:04 -0300

    Incluir un modulo de datos que contenga info de los productos

--- ProyectoIntegrador/db/datos.js
+++ ProyectoIntegrador/db/datos.js
@@ -1,272 +1,277 @@
 let datosApp = {
-    usuario: {
-      email: "juanperez@mail.com",
-      usuario: "Juan Pérez",
-      contraseña: "12345678",
-      fechaNacimiento: "1990-01-01",
-      nroDocumento: 12345678,
-      fotoPerfil: "/imagenes/perfiles/usuario123.jpg",
+  usuario: {
+    email: "juanperez@mail.com",
+    usuario: "Juan Pérez",
+    contraseña: "12345678",
+    fechaNacimiento: "1990-01-01",
+    nroDocumento: 12345678,
+    fotoPerfil: "/imagenes/perfiles/usuario123.jpg",
+  },
+
+  productos: [
+      {
+          imagen: "/images/products/EtiosHatchback.jpg",
+          nombre: "Etios Hatchback",
+          descripcion: "Caja manual de 6 velocidades, Control de estabilidad (VSC) y tracción (TRC), Pack eléctrico",
+          comentarios: [
+            {
+              nombreUsuario: "Usuario Ejemplo",
+              texto: "¡Me encanta este producto!",
+              imagenPerfil: "/imagenes/perfiles/ejemploPerfil1.jpg"
+            },
+            {
+              nombreUsuario: "Usuario Ejemplo",
+              texto: "¡Me fascina este producto!",
+              imagenPerfil: "/imagenes/perfiles/ejemploPerfil1.jpg"
+            },
+
+            {
+              nombreUsuario: "Usuario Ejemplo",
+              texto: "¡Me divierte este producto!",
+              imagenPerfil: "/imagenes/perfiles/ejemploPerfil1.jpg"
+            }
+          ]},
+
+
+    {
+      imagen: "/images/products/EtiosSedan.jpg",
+      nombre: "Etios Sedán",
+      descripcion: "Ofrece llantas de aleación diamantadas y faros oscurecidos que le dan una impronta más agresiva y robusta.",
+      comentarios: [
+          {
+            nombreUsuario: "Usuario Ejemplo",
+            texto: "¡Me encanta este producto!",
+            imagenPerfil: "/imagenes/perfiles/ejemploPerfil1.jpg"
+          },
+          {
+            nombreUsuario: "Usuario Ejemplo",
+            texto: "¡Me fascina este producto!",
+            imagenPerfil: "/imagenes/perfiles/ejemploPerfil1.jpg"
+          },
+
+          {
+            nombreUsuario: "Usuario Ejemplo",
+            texto: "¡Me divierte este producto!",
+            imagenPerfil: "/imagenes/perfiles/ejemploPerfil1.jpg"
+          }
+        ]
     },
 
-    productos: [
-        {
-            imagen: "/images/products/default-image.png",
-            nombre: "Producto 1",
-            descripcion: "Descripción del producto 1.",
-            comentarios: [
-              {
-                nombreUsuario: "Usuario Ejemplo",
-                texto: "¡Me encanta este producto!",
-                imagenPerfil: "/imagenes/perfiles/ejemploPerfil1.jpg"
-              },
-              {
-                nombreUsuario: "Usuario Ejemplo",
-                texto: "¡Me fascina este producto!",
-                imagenPerfil: "/imagenes/perfiles/ejemploPerfil1.jpg"
-              },
+    {
+      imagen: "/images/products/YarisHatchback.png",
+      nombre: "Yaris Hatchback",
+      descripcion: "Audio con pantalla táctil LCD de 7” con conectividad: Apple CarPlay® & Android Auto®*, Asiento del conductor con regulación en altura y volante multifunción.",
+      comentarios: [
+          {
+            nombreUsuario: "Usuario Ejemplo",
+            texto: "¡Me encanta este producto!",
+            imagenPerfil: "/imagenes/perfiles/ejemploPerfil1.jpg"
+          },
+          {
+            nombreUsuario: "Usuario Ejemplo",
+            texto: "¡Me fascina este producto!",
+            imagenPerfil: "/imagenes/perfiles/ejemploPerfil1.jpg"
+          },
 
-              {
-                nombreUsuario: "Usuario Ejemplo",
-                texto: "¡Me divierte este producto!",
-                imagenPerfil: "/imagenes/perfiles/ejemploPerfil1.jpg"
-              }
-            ]},
+          {
+            nombreUsuario: "Usuario Ejemplo",
+            texto: "¡Me divierte este producto!",
+            imagenPerfil: "/imagenes/perfiles/ejemploPerfil1.jpg"
+          }
+        ]
+    },
+    {
+      imagen: "/images/products/Corolla2.jpg",
+      nombre: "Corolla",
+      descripcion: "Caja automática Direct Shift CVT de 10 velocidades pre-programadas.",
+      comentarios: [
+          {
+            nombreUsuario: "Usuario Ejemplo",
+            texto: "¡Me encanta este producto!",
+            imagenPerfil: "/imagenes/perfiles/ejemploPerfil1.jpg"
+          },
+          {
+            nombreUsuario: "Usuario Ejemplo",
+            texto: "¡Me fascina este producto!",
+            imagenPerfil: "/imagenes/perfiles/ejemploPerfil1.jpg"
+          },
 
+          {
+            nombreUsuario: "Usuario Ejemplo",
+            texto: "¡Me divierte este producto!",
+            imagenPerfil: "/imagenes/perfiles/ejemploPerfil1.jpg"
+          }
+        ]
+    },
+    {
+      imagen: "/images/products/PartnerPatagonica.jpg",
+      nombre: "PARTNER PATAGÓNICA",
+      descripcion: "Frenos ABS con Repartidor Electrónico de Frenado, Doble Airbag frontal, Carrocería con zonas de deformación preestablecidas y frente pensado para proteger al peatón",
+      comentarios: [
+          {
+            nombreUsuario: "Usuario Ejemplo",
+            texto: "¡Me encanta este producto!",
+            imagenPerfil: "/imagenes/perfiles/ejemploPerfil1.jpg"
+          },
+          {
+            nombreUsuario: "Usuario Ejemplo",
+            texto: "¡Me fascina este producto!",
+            imagenPerfil: "/imagenes/perfiles/ejemploPerfil1.jpg"
+          },
 
-      {
-        imagen: "/imagenes/productos/prod2.jpg",
-        nombre: "Producto 2",
-        descripcion: "Descripción del producto 2.",
-        comentarios: [
-            {
-              nombreUsuario: "Usuario Ejemplo",
-              texto: "¡Me encanta este producto!",
-              imagenPerfil: "/imagenes/perfiles/ejemploPerfil1.jpg"
-            },
-            {
-              nombreUsuario: "Usuario Ejemplo",
-              texto: "¡Me fascina este producto!",
-              imagenPerfil: "/imagenes/perfiles/ejemploPerfil1.jpg"
-            },
+          {
+            nombreUsuario: "Usuario Ejemplo",
+            texto: "¡Me divierte este producto!",
+            imagenPerfil: "/imagenes/perfiles/ejemploPerfil1.jpg"
+          }
+        ]
+    },
+    {
+      imagen: "/images/products/Suv3008.jpg",
+      nombre: "SUV 3008",
+      descripcion: "Cuenta con una nueva parrilla diamantada, luces diurnas con forma de colmillos y nuevos faros FULL LED felinos y refinados",
+      comentarios: [
+          {
+            nombreUsuario: "Usuario Ejemplo",
+            texto: "¡Me encanta este producto!",
+            imagenPerfil: "/imagenes/perfiles/ejemploPerfil1.jpg"
+          },
+          {
+            nombreUsuario: "Usuario Ejemplo",
+            texto: "¡Me fascina este producto!",
+            imagenPerfil: "/imagenes/perfiles/ejemploPerfil1.jpg"
+          },
 
-            {
-              nombreUsuario: "Usuario Ejemplo",
-              texto: "¡Me divierte este producto!",
-              imagenPerfil: "/imagenes/perfiles/ejemploPerfil1.jpg"
-            }
-          ]
-      },
+          {
+            nombreUsuario: "Usuario Ejemplo",
+            texto: "¡Me divierte este producto!",
+            imagenPerfil: "/imagenes/perfiles/ejemploPerfil1.jpg"
+          },
+          {
+            nombreUsuario: "Usuario Ejemplo",
+            texto: "¡Me divierte este producto!",
+            imagenPerfil: "/imagenes/perfiles/ejemploPerfil1.jpg"
+          }
+        ]
+    },
+    {
+      imagen: "/images/products/Peugot208.jpeg",
+      nombre: "PEUGEOT 208",
+      descripcion: "Nuevo motor ultra eficiente de 3 cilindros con consumos bajos ideal para un uso urbano y citadino.",
+      comentarios: [
+          {
+            nombreUsuario: "Usuario Ejemplo",
+            texto: "¡Me encanta este producto!",
+            imagenPerfil: "/imagenes/perfiles/ejemploPerfil1.jpg"
+          },
+          {
+            nombreUsuario: "Usuario Ejemplo",
+            texto: "¡Me fascina este producto!",
+            imagenPerfil: "/imagenes/perfiles/ejemploPerfil1.jpg"
+          },
 
-      {
-        imagen: "/imagenes/productos/prod3.jpg",
-        nombre: "Producto 3",
-        descripcion: "Descripción del producto 3.",
-        comentarios: [
-            {
-              nombreUsuario: "Usuario Ejemplo",
-              texto: "¡Me encanta este producto!",
-              imagenPerfil: "/imagenes/perfiles/ejemploPerfil1.jpg"
-            },
-            {
-              nombreUsuario: "Usuario Ejemplo",
-              texto: "¡Me fascina este producto!",
-              imagenPerfil: "/imagenes/perfiles/ejemploPerfil1.jpg"
-            },
+          {
+            nombreUsuario: "Usuario Ejemplo",
+            texto: "¡Me divierte este producto!",
+            imagenPerfil: "/imagenes/perfiles/ejemploPerfil1.jpg"
+          },
+          {
+            nombreUsuario: "Usuario Ejemplo",
+            texto: "¡Me divierte este producto!",
+            imagenPerfil: "/imagenes/perfiles/ejemploPerfil1.jpg"
+          }
+        ]
+    },
+    {
+      imagen: "/images/products/EXPERT.webp",
+      nombre: "PEUGEOT EXPERT",
+      descripcion: "El rendimiento está asegurado por un motor Diésel, moderno y eficiente, capaz de entregar 115 CV de potencia y 300 Nm de torque, asociado a una transmisión manual de 6 velocidades",
+      comentarios: [
+          {
+            nombreUsuario: "Usuario Ejemplo",
+            texto: "¡Me encanta este producto!",
+            imagenPerfil: "/imagenes/perfiles/ejemploPerfil1.jpg"
+          },
+          {
+            nombreUsuario: "Usuario Ejemplo",
+            texto: "¡Me fascina este producto!",
+            imagenPerfil: "/imagenes/perfiles/ejemploPerfil1.jpg"
+          },
 
-            {
-              nombreUsuario: "Usuario Ejemplo",
-              texto: "¡Me divierte este producto!",
-              imagenPerfil: "/imagenes/perfiles/ejemploPerfil1.jpg"
-            }
-          ]
-      },
-      {
-        imagen: "/imagenes/productos/prod4.jpg",
-        nombre: "Producto 4",
-        descripcion: "Descripción del producto 4.",
-        comentarios: [
-            {
-              nombreUsuario: "Usuario Ejemplo",
-              texto: "¡Me encanta este producto!",
-              imagenPerfil: "/imagenes/perfiles/ejemploPerfil1.jpg"
-            },
-            {
-              nombreUsuario: "Usuario Ejemplo",
-              texto: "¡Me fascina este producto!",
-              imagenPerfil: "/imagenes/perfiles/ejemploPerfil1.jpg"
-            },
+          {
+            nombreUsuario: "Usuario Ejemplo",
+            texto: "¡Me divierte este producto!",
+            imagenPerfil: "/imagenes/perfiles/ejemploPerfil1.jpg"
+          },
+          {
+            nombreUsuario: "Usuario Ejemplo",
+            texto: "¡Me divierte este producto!",
+            imagenPerfil: "/imagenes/perfiles/ejemploPerfil1.jpg"
+          }
+        ]
+    },
+    {
+      imagen: "/images/products/citroenc5.jpg",
+      nombre: "C5 AIRCROSS",
+      descripcion: "Motor naftero THP de alto rendimiento que otorgará una potencia de 165 CV, con un par motor de 240 Nm, asociado a una caja automática EAT6 de seis velocidades, optimizando la relación de transmisión entre el motor y la caja.",
+      comentarios: [
+          {
+            nombreUsuario: "Usuario Ejemplo",
+            texto: "¡Me encanta este producto!",
+            imagenPerfil: "/imagenes/perfiles/ejemploPerfil1.jpg"
+          },
+          {
+            nombreUsuario: "Usuario Ejemplo",
+            texto: "¡Me fascina este producto!",
+            imagenPerfil: "/imagenes/perfiles/ejemploPerfil1.jpg"
+          },
 
-            {
-              nombreUsuario: "Usuario Ejemplo",
-              texto: "¡Me divierte este producto!",
-              imagenPerfil: "/imagenes/perfiles/ejemploPerfil1.jpg"
-            }
-          ]
-      },
-      {
-        imagen: "/imagenes/productos/prod5.jpg",
-        nombre: "Producto 5",
-        descripcion: "Descripción del producto 5.",
-        comentarios: [
-            {
-              nombreUsuario: "Usuario Ejemplo",
-              texto: "¡Me encanta este producto!",
-              imagenPerfil: "/imagenes/perfiles/ejemploPerfil1.jpg"
-            },
-            {
-              nombreUsuario: "Usuario Ejemplo",
-              texto: "¡Me fascina este producto!",
-              imagenPerfil: "/imagenes/perfiles/ejemploPerfil1.jpg"
-            },
+          {
+            nombreUsuario: "Usuario Ejemplo",
+            texto: "¡Me divierte este producto!",
+            imagenPerfil: "/imagenes/perfiles/ejemploPerfil1.jpg"
+          },
+          {
+            nombreUsuario: "Usuario Ejemplo",
+            texto: "¡Me divierte este producto!",
+            imagenPerfil: "/imagenes/perfiles/ejemploPerfil1.jpg"
+          }
+        ]
+    },
+    {
+      imagen: "/images/products/C4CACTUS.jpeg",
+      nombre: "C4 CACTUS",
+      descripcion: "Llantas 16”, Citroën connect 7”, Volante multifunción, Regulador y limitador de velocidad",
+      comentarios: [
+          {
+            nombreUsuario: "Usuario Ejemplo",
+            texto: "¡Me encanta este producto!",
+            imagenPerfil: "/imagenes/perfiles/ejemploPerfil1.jpg"
+          },
+          {
+            nombreUsuario: "Usuario Ejemplo",
+            texto: "¡Me fascina este producto!",
+            imagenPerfil: "/imagenes/perfiles/ejemploPerfil1.jpg"
+          },
 
-            {
-              nombreUsuario: "Usuario Ejemplo",
-              texto: "¡Me divierte este producto!",
-              imagenPerfil: "/imagenes/perfiles/ejemploPerfil1.jpg"
-            }
-          ]
-      },
-      {
-        imagen: "/imagenes/productos/prod6.jpg",
-        nombre: "Producto 6",
-        descripcion: "Descripción del producto 6.",
-        comentarios: [
-            {
-              nombreUsuario: "Usuario Ejemplo",
-              texto: "¡Me encanta este producto!",
-              imagenPerfil: "/imagenes/perfiles/ejemploPerfil1.jpg"
-            },
-            {
-              nombreUsuario: "Usuario Ejemplo",
-              texto: "¡Me fascina este producto!",
-              imagenPerfil: "/imagenes/perfiles/ejemploPerfil1.jpg"
-            },
-
-            {
-              nombreUsuario: "Usuario Ejemplo",
-              texto: "¡Me divierte este producto!",
-              imagenPerfil: "/imagenes/perfiles/ejemploPerfil1.jpg"
-            }
-          ]
-      },
-      {
-        imagen: "/imagenes/productos/prod7.jpg",
-        nombre: "Producto 7",
-        descripcion: "Descripción del producto 7.",
-        comentarios: [
-            {
-              nombreUsuario: "Usuario Ejemplo",
-              texto: "¡Me encanta este producto!",
-              imagenPerfil: "/imagenes/perfiles/ejemploPerfil1.jpg"
-            },
-            {
-              nombreUsuario: "Usuario Ejemplo",
-              texto: "¡Me fascina este producto!",
-              imagenPerfil: "/imagenes/perfiles/ejemploPerfil1.jpg"
-            },
-
-            {
-              nombreUsuario: "Usuario Ejemplo",
-              texto: "¡Me divierte este producto!",
-              imagenPerfil: "/imagenes/perfiles/ejemploPerfil1.jpg"
-            },
-            {
-              nombreUsuario: "Usuario Ejemplo",
-              texto: "¡Me fascina este producto!",
-              imagenPerfil: "/imagenes/perfiles/ejemploPerfil1.jpg"
-            }
-          ]
-      },
-      {
-        imagen: "/imagenes/productos/prod8.jpg",
-        nombre: "Producto 8",
-        descripcion: "Descripción del producto 8.",
-        comentarios: [
-            {
-              nombreUsuario: "Usuario Ejemplo",
-              texto: "¡Me encanta este producto!",
-              imagenPerfil: "/imagenes/perfiles/ejemploPerfil1.jpg"
-            },
-            {
-              nombreUsuario: "Usuario Ejemplo",
-              texto: "¡Me fascina este producto!",
-              imagenPerfil: "/imagenes/perfiles/ejemploPerfil1.jpg"
-            },
-
-            {
-              nombreUsuario: "Usuario Ejemplo",
-              texto: "¡Me divierte este producto!",
-              imagenPerfil: "/imagenes/perfiles/ejemploPerfil1.jpg"
-            },
-            {
-              nombreUsuario: "Usuario Ejemplo",
-              texto: "¡Me divierte este producto!",
-              imagenPerfil: "/imagenes/perfiles/ejemploPerfil1.jpg"
-            }
-          ]
-      },
-      {
-        imagen: "/imagenes/productos/9.jpg",
-        nombre: "Producto 9",
-        descripcion: "Descripción del producto 9.",
-        comentarios: [
-            {
-              nombreUsuario: "Usuario Ejemplo",
-              texto: "¡Me encanta este producto!",
-              imagenPerfil: "/imagenes/perfiles/ejemploPerfil1.jpg"
-            },
-            {
-              nombreUsuario: "Usuario Ejemplo",
-              texto: "¡Me fascina este producto!",
-              imagenPerfil: "/imagenes/perfiles/ejemploPerfil1.jpg"
-            },
-
-            {
-              nombreUsuario: "Usuario Ejemplo",
-              texto: "¡Me divierte este producto!",
-              imagenPerfil: "/imagenes/perfiles/ejemploPerfil1.jpg"
-            },
-            {
-              nombreUsuario: "Usuario Ejemplo",
-              texto: "¡Me divierte este producto!",
-              imagenPerfil: "/imagenes/perfiles/ejemploPerfil1.jpg"
-            }
-          ]
-      },
-      {
-        imagen: "/imagenes/productos/prod10.jpg",
-        nombre: "Producto 10",
-        descripcion: "Descripción del producto 10.",
-        comentarios: [
-            {
-              nombreUsuario: "Usuario Ejemplo",
-              texto: "¡Me encanta este producto!",
-              imagenPerfil: "/imagenes/perfiles/ejemploPerfil1.jpg"
-            },
-            {
-              nombreUsuario: "Usuario Ejemplo",
-              texto: "¡Me fascina este producto!",
-              imagenPerfil: "/imagenes/perfiles/ejemploPerfil1.jpg"
-            },
-
-            {
-              nombreUsuario: "Usuario Ejemplo",
-              texto: "¡Me divierte este producto!",
-              imagenPerfil: "/imagenes/perfiles/ejemploPerfil1.jpg"
-            },
-            {
-              nombreUsuario: "Usuario Ejemplo",
-              texto: "¡Me divierte este producto!",
-              imagenPerfil: "/imagenes/perfiles/ejemploPerfil1.jpg"
-            }
-          ]
-      },
-    
-
-
-    ],
-
-
-  };
+          {
+            nombreUsuario: "Usuario Ejemplo",
+            texto: "¡Me divierte este producto!",
+            imagenPerfil: "/imagenes/perfiles/ejemploPerfil1.jpg"
+          },
+          {
+            nombreUsuario: "Usuario Ejemplo",
+            texto: "¡Me divierte este producto!",
+            imagenPerfil: "/imagenes/perfiles/ejemploPerfil1.jpg"
+          }
+        ]
+    },
   
-  module.exports = datosApp;
+
+
+  ],
+
+
+};
+
+module.exports = datosApp;
