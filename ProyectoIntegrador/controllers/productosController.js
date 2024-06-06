eba5bbc1cf39ac2a9250184b48b3745206971c73
victoriaelizalde <velizalde@udesa.edu.ar>
2024-06-06 10:19:29 -0300

    Crear Modelo Product y sus relaciones. (Requiere Sequelize)

--- ProyectoIntegrador/controllers/productosController.js
+++ ProyectoIntegrador/controllers/productosController.js
@@ -1,16 +1,58 @@
-const datos = require('../db/datos');
+const db = require('../database/models');
 
 const productosController = {
-    product: function (req,res) {
-        return res.render('product', {lista: datos} )
-    },
-    productAdd: function (req,res) {
-        return res.render('product-add', {lista: datos} )
+    index: function (req,res) {
+        db.Producto.findAll({
+            include: [
+                {association: "Usuario"},
+                {association: "Comentario"}
+]
+})
+
+    .then(function(resultado) {
+
+        return res.render("index",{lista: resultado})
+
+    }).catch(function(errores) {
+
+        return console.log(errores);;
+})
+},
+
+product: function (req,res) {
+
+    return res.render('product', {lista: datos} )
+},
+
+productAdd: function (req,res) {
+
+    return res.render('product-add', {lista: datos} )
     },
 
-    search: function(req, res, next) {
-        return res.render('search-results', {lista: datos} )
-      }
+productInfo: function (req,res) {
 
+    let id = req.params.id
+    db.Producto.findByPk( id, {
+        include: [
+            {association: "Usuario"},
+            {
+                association: "Comentario",
+                include: [{association: "Usuario"}]
+} ]
+})
+
+    .then(function(resultado) {
+        return res.render('product',{lista: resultado})
+
+    }).catch(function(errores) {
+        return console.log(errores);;
+})
+},
+
+search: function(req, res, next) {
+    return res.render('product', {lista: datos} )
+}
 };
+
 module.exports= productosController;
+
