a93faf1d9ba19f32e460968a37fc0051bf21fad3
nico-dotcom <nicolasrusso004@gmail.com>
2024-06-20 12:24:52 -0300

    Consigna punto 11 - Editar mi usuario: desarrollo completo + validaciones y control de usuario de edición

--- ProyectoIntegrador/controllers/userController.js
+++ ProyectoIntegrador/controllers/userController.js
@@ -7,7 +7,6 @@ const userController = {
     profile: function (req, res) {
 
         let id = req.params.id
-        console.log("ID:", id);
         db.Usuario.findByPk(id, {
             include: [
                 {
@@ -33,7 +32,78 @@ const userController = {
     },
 
     profileEdit: function (req, res) {
-        return res.render('profile-edit', { lista: user })
+        if (req.session.user != undefined) {
+
+        let id_url = req.params.id;
+        const userId_session = res.locals.user.id_usuario;
+
+        if (id_url == userId_session) {
+
+            db.Usuario.findByPk(id_url)
+                .then(function (resultado) {
+                    return res.render("profile-edit", { lista: resultado })
+
+                }).catch(function (errores) {
+                    return console.log(errores);;
+                })
+
+        } else {
+            return res.send('Solo puedes editar tu usuario, porfavor vuelve al inicio')
+        }
+    } else {
+        return res.redirect('/user/login');
+    }
+
+    },profileUpdate:function (req, res) {
+        let form = req.body;
+        let idUsuario = res.locals.user.id_usuario;
+        let contrasenaSession = res.locals.user.contrasena;
+        let errors = validationResult(req);
+
+        if(form.pass == ""){
+             contrasenaNueva = contrasenaSession;
+        }else{
+             contrasenaNueva = bcrypt.hashSync(form.pass, 10)
+        }
+
+        let filtro = {
+            where: [{ id_usuario: idUsuario }]
+        };
+
+        let nuevoProducto = {
+            nombre_usuario: form.nombre_usuario,
+            email: form.email,
+            contrasenas: contrasenaNueva,
+            fecha: form.fecha,
+            dni: form.dni,
+            foto: form.foto
+        }
+
+        if (errors.isEmpty()) {
+
+        db.Usuario.update(nuevoProducto, filtro)
+            .then((result) => {
+                return res.redirect("/");
+            }).catch((err) => {
+                return console.log(err);
+            });
+
+        }else {
+            
+        db.Usuario.findByPk(idUsuario)
+        .then(function (resultado) {
+            return res.render('profile-edit', {
+                errors: errors.mapped(),
+                old: req.body,
+                lista: resultado });
+
+        }).catch(function (errores) {
+            return console.log(errores);;
+        })
+
+};
+
+
     },
 
     login: function (req,res) {
@@ -66,7 +136,6 @@ const userController = {
             if (check) {
                 req.session.user = result;
 
-                /* que lo guarde en cookie si el usuario lo tildo */
                 if (form.rememberme != undefined) {
                     res.cookie("userId", result.id_usuario, {maxAge: 1000 * 60 * 15});
                 }
@@ -87,7 +156,6 @@ const userController = {
         let errors = validationResult(req);
         if (errors.isEmpty()) {
             
-            /*capturo la info del form*/
             let form = req.body;
 
             let user = {
